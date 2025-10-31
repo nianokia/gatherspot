7ff90e71c1ad33ee74b4b10dc16c02c144f7804d
Nia Wright <nw.niawright@gmail.com>
2025-10-31 04:01:57 -0400

    feat(admin): Outline admin dashboard features

--- client/src/pages/AdminDash.jsx
+++ client/src/pages/AdminDash.jsx
@@ -8,6 +8,36 @@ const AdminDash = () => {
     <div className="AdminDash">
       <BackButton />
       <h1>Admin Dashboard</h1>
+      <h2>All Events</h2>
+      <p>Search, sort, and filter events</p>
+      <button>
+        Create New Event
+      </button>
+      {/* --- List of events would go here --- */}
+      <ul className='userEventList'>
+        <li>Clickable Events
+          <ul>
+            <li className='userEvent'>GET event</li>
+            <li className="userEvent">UPDATE event (venue, ticket_types, waitlist)</li>
+            <li className="userEvent">GET sessions</li>
+            <li className="userEvent">CREATE session (speakers, vendors)</li>
+            <li className="userEvent">CREATE notifications (schedule changes, venue updates, important announcements)</li>
+            <li className="userEvent">DELETE event</li>
+          </ul>
+        </li>
+      </ul>
+      <button>
+        View Event Analytics
+      </button>
+      <ul className='userEventList'>
+        <li>Clickable Events
+          <ul>
+            <li className='userEvent'>GET event_metrics</li>
+            <li className="userEvent">GET feedback</li>
+            <li className="userEvent">EXPORT report as a file</li>
+          </ul>
+        </li>
+      </ul>
       <button className='logoutBtn' onClick={logout}>Logout</button>
     </div>
   );
