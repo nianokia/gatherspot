9bf35c61a888f4068f901bf180df18e27117478a
Nia Wright <nw.niawright@gmail.com>
2025-10-31 03:56:40 -0400

    feat(vendor): Outline vendor dashboard features

--- client/src/pages/VendorDash.jsx
+++ client/src/pages/VendorDash.jsx
@@ -8,6 +8,19 @@ const VendorDash = () => {
     <div className="VendorDash">
       <BackButton />
       <h1>Vendor Dashboard</h1>
+      <h2>All Events</h2>
+      <p>Search, sort, and filter events</p>
+      <h2>My Events</h2>
+      {/* --- List of events would go here --- */}
+      <ul className='userEventList'>
+        <li>Clickable Events
+          <ul>
+            <li className='userEvent'>GET event</li>
+            <li className="userEvent">GET sessions</li>
+            <li className="userEvent">GET notifications (schedule changes, venue updates, important announcements)</li>
+          </ul>
+        </li>
+      </ul>
       <button className='logoutBtn' onClick={logout}>Logout</button>
     </div>
   );
