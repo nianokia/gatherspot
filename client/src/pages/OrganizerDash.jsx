3b6d6adb662876cd81d314388881fc79f20fb23a
Nia Wright <nw.niawright@gmail.com>
2025-10-31 03:39:57 -0400

    feat(organizer): Outline organizer dashboard features

--- client/src/pages/OrganizerDash.jsx
+++ client/src/pages/OrganizerDash.jsx
@@ -8,6 +8,36 @@ const OrganizerDash = () => {
     <div className="OrganizerDash">
       <BackButton />
       <h1>Organizer Dashboard</h1>
+      <button>
+        Create New Event
+      </button>
+      <h2>My Events</h2>
+      {/* --- List of events would go here --- */}
+      <ul className='userEventList'>
+        <li>Clickable Events
+          <ul>
+            <li className='userEvent'>GET event</li>
+            <li className="userEvent">UPDATE event (venue, ticket_types, waitlist)</li>
+            <li className="userEvent">GET sessions</li>
+            <li className="userEvent">CREATE session (speakers, vendors)</li>
+            <li className="userEvent">CREATE notifications (schedule changes, venue updates, important announcements)</li>
+            <li className="userEvent">DELETE event</li>
+          </ul>
+        </li>
+      </ul>
+      <button>
+        View Event Analytics
+      </button>
+      <ul className='userEventList'>
+        <li>Clickable Events
+          <ul>
+            <li className='userEvent'>GET event_metrics</li>
+            <li className="userEvent">GET feedback</li>
+            <li className="userEvent">EXPORT report as a file</li>
+          </ul>
+        </li>
+      </ul>
+      <br />
       <button className='logoutBtn' onClick={logout}>Logout</button>
     </div>
   );
