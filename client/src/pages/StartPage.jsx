6c297f7b8fe6d2c9c77a2f8ac7d5427d80af7f5f
Nia Wright <nw.niawright@gmail.com>
2025-10-31 19:28:41 -0400

    feat(start page): conditionally render AllEvents component if user is logged in

--- client/src/pages/StartPage.jsx
+++ client/src/pages/StartPage.jsx
@@ -1,12 +1,25 @@
+import { useContext } from "react";
+import AuthContext from "../context/authContext";
+import AllEvents from "../components/AllEvents";
 import AuthHub from "../components/AuthHub";
 
 const StartPage = () => {
+  const { user } = useContext(AuthContext);
   return (
     <div className="AppContainer">
-      <h1>Welcome to GatherSpot!</h1>
-      <h2>Your one-stop spot for planning, organizing, and finding events.</h2>
-      <h3>Get started by creating an account or logging in.</h3>
-      <AuthHub />
+      {user ? (
+        <div>
+          {/* --- DISPLAY ALL EVENTS --- */}
+          <AllEvents />
+        </div>
+      ) : (
+        <>
+          <h1>Welcome to GatherSpot!</h1>
+          <h2>Your one-stop spot for planning, organizing, and finding events.</h2>
+          <h3>Get started by creating an account or logging in.</h3>
+          <AuthHub />
+        </>
+      )}
     </div>
   )
 }
