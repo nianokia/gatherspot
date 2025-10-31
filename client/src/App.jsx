f468c135ea3c9b7a6d9ba947cc0229c7552ecb81
Nia Wright <nw.niawright@gmail.com>
2025-10-31 01:06:22 -0400

    feat(app): create StartPage & update routing in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,22 @@
-import { useState } from 'react'
 import gsLogo from '/gatherspot-logo.png'
-import gsIcon from '/gatherspot-icon.png'
+import { Routes, Route, Link } from 'react-router'
 import './App.css'
 
-import AuthHub from './components/AuthHub.jsx'
+import StartPage from './pages/StartPage.jsx'
+import SignUp from './pages/SignUp.jsx'
 
 function App() {
   return (
     <div className='App'>
       <nav className='navBar'>
         <img src={gsLogo} className="logo" alt="GatherSpot logo" />
-        <a href="/">Sign Up</a>
+        <Link to="/signup">Sign Up</Link>
       </nav>
       <div className="AppContainer">
-        <h1>Welcome to GatherSpot!</h1>
-        <h2>Your one-stop spot for planning, organizing, and finding events.</h2>
-        <h3>Get started by creating an account or logging in.</h3>
-        <AuthHub />
+        <Routes>
+          <Route path="/" element={<StartPage />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
       </div>
     </div>
   )
---
+++ client/src/pages/StartPage.jsx
@@ -0,0 +1,14 @@
+import AuthHub from "../components/AuthHub";
+
+const StartPage = () => {
+  return (
+    <div className="AppContainer">
+      <h1>Welcome to GatherSpot!</h1>
+      <h2>Your one-stop spot for planning, organizing, and finding events.</h2>
+      <h3>Get started by creating an account or logging in.</h3>
+      <AuthHub />
+    </div>
+  )
+}
+
+export default StartPage;
