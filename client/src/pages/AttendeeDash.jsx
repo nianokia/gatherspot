e84eaf43c2966b77a1072ec074a54ee71e627c8a
Nia Wright <nw.niawright@gmail.com>
2025-10-31 03:52:43 -0400

    feat(attendee): Outline attendee dashboard features

--- client/src/pages/AttendeeDash.jsx
+++ client/src/pages/AttendeeDash.jsx
@@ -8,6 +8,26 @@ const AttendeeDash = () => {
     <div className="AttendeeDash">
       <BackButton />
       <h1>Attendee Dashboard</h1>
+      <h2>All Events</h2>
+      <p>Search, sort, and filter events</p>
+      <button>Buy Ticket</button>
+      <ul className="userEventList">
+        <li className="userEvent">CREATE QR code ticket</li>
+        <li className="userEvent">CREATE Payment</li>
+      </ul>
+      <h2>My Events</h2>
+      {/* --- List of events would go here --- */}
+      <ul className='userEventList'>
+        <li>Clickable Events
+          <ul>
+            <li className='userEvent'>GET event</li>
+            <li className="userEvent">UPDATE registration</li>
+            <li className="userEvent">GET sessions</li>
+            <li className="userEvent">GET QR code tickets</li>
+            <li className="userEvent">GET notifications (schedule changes, venue updates, important announcements)</li>
+          </ul>
+        </li>
+      </ul>
       <button className='logoutBtn' onClick={logout}>Logout</button>
     </div>
   );
