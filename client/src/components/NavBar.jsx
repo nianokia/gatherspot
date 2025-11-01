34050c047e26813dc4d04c713378ef8929a4014d
Nia Wright <nw.niawright@gmail.com>
2025-10-31 20:29:32 -0400

    feat(navbar): implement nav menu functionality to show/ hide user links

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -7,25 +7,52 @@ import AuthContext from '../context/authContext.jsx';
 
 const NavBar = () => {
   const { user, logout } = useContext(AuthContext);
+
+  // ----------- OPEN NAV MENU -----------
+  const openMenu = () => {
+    const navLinks = document.querySelector('.navLinks');
+    if (navLinks.classList) {
+      navLinks.classList.toggle('open');
+    }
+  };
+
   return (
     <nav className='navBar'>
-      {/* --- conditionally navigate based on user role --- */}
-      {/* <Link to={user ? (user.role_id === 1 ? '/organizer' 
-        : user.role_id === 2 ? '/attendee' 
-        : user.role_id === 3 ? '/vendor' 
-        : '/admin') 
-        : '/'
-      }> */}
       <Link to="/">
         <img src={gsLogo} className="logo" alt="GatherSpot logo" />
       </Link>
-      {/* --- conditionally render logout button or sign up link --- */}
+      {/* --- conditionally render menu navLinks based on if user exists and their role --- */}
       {user ? (
         <div className="navGroup">
           <button onClick={logout} className="logoutBtn">Logout</button>
-          <FontAwesomeIcon icon={faBars} className='menuIcon'/>
+          <div className="navLinks">
+            {user.role_id === 1 && (
+              <>
+                <Link to="/organizer">Dashboard</Link>
+                <Link to="/add-event">Add Event</Link>
+                <Link to="/add-venue">Add Venue</Link>
+              </>
+            )}
+            {user.role_id === 2 && (
+              <>
+                <Link to="/attendee">Dashboard</Link>
+              </>
+            )}
+            {user.role_id === 3 && (
+              <>
+                <Link to="/vendor">Dashboard</Link>
+              </>
+            )}
+            {user.role_id === 4 && (
+              <>
+                <Link to="/admin">Dashboard</Link>
+                <Link to="/add-event">Add Event</Link>
+                <Link to="/add-venue">Add Venue</Link>
+              </>
+            )}
+          </div>
+          <FontAwesomeIcon icon={faBars} className='menuIcon' onClick={openMenu}/>
         </div>
-        
       ) : (
         <Link to="/signup">Sign Up</Link>
       )}
