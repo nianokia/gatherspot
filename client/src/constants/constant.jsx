01f0ad46f86c82bdd4f68721aca471f4bc989c6f
Nia Wright <nw.niawright@gmail.com>
2025-11-02 00:08:39 -0400

    feat(modal): create Modal & ConfirmModal as exportable components

--- client/src/constants/constant.jsx
+++ client/src/constants/constant.jsx
@@ -14,6 +14,55 @@ export const BackButton = () => {
   )
 }
 
+export const Modal = ({ isOpen, onClose, children }) => {
+  if (!isOpen) return null;
+
+  return (
+    // --- Close modal when clicking outside modal ---
+    <div className="modal-overlay" onClick={onClose}>
+      {/* --- Prevent closing when clicking inside modal --- */}
+      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+        <button className="modal-close" onClick={onClose}>&times;</button>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export const ConfirmModal = ({
+  isOpen,
+  onClose,
+  onConfirm,
+  title = "Are you sure you want to proceed?",
+  message = "This action is permanent and can't be undone.",
+  confirmText = "Confirm",
+  cancelText = "Cancel",
+  cancelColor = "#d9534f"
+}) => {
+  if (!isOpen) return null;
+
+  const handleConfirm = async () => {
+    await onConfirm();
+    // --- close modal after confirmation ---
+    onClose();
+  }
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <h3>{title}</h3>
+      <p>{message}</p>
+      <div className="modal-actions">
+        <button onClick={handleConfirm} className="confirmBtn">
+          {confirmText}
+        </button>
+        <button onClick={onClose} className="cancelBtn" style={{ backgroundColor: cancelColor}}>
+          {cancelText}
+        </button>
+      </div>
+    </Modal>
+  )
+}
+
 export const formatDate = (date) => {
   // --- define options for formatting ---
   const options = {
