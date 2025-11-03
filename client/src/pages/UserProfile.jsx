8a722c898b35b0a6855130a2bf0f106be6750ebd
Nia Wright <nw.niawright@gmail.com>
2025-11-02 19:08:51 -0500

    feat(profile): add user profile to view account details

---
+++ client/src/pages/UserProfile.jsx
@@ -0,0 +1,37 @@
+import { useContext } from "react";
+import AuthContext from "../context/authContext.jsx";
+import EditVendorProfile from "./EditVendorProfile.jsx";
+import { BackButton } from "../constants/constant.jsx";
+
+const UserProfile = () => {
+  const { user, token } = useContext(AuthContext);
+  const roleId = user?.role_id;
+  return (
+    <div className="UserProfile">
+      <BackButton />
+      <h2>My Profile</h2>
+      <ul className="profileDetails">
+        <li>
+          Name: {user?.f_name} {user?.l_name}
+        </li>
+        <li>
+          Email: {user?.email}
+        </li>
+        <li>
+          Phone: {user?.phone || "N/A"}
+        </li>
+        <li>
+          User ID: {user?.id}
+        </li>
+        <li>
+          Role: {user?.role}
+        </li>
+      </ul>
+      {roleId === 3 && (
+        <EditVendorProfile userId={user?.id} token={token} />
+      )}
+    </div>
+  );
+};
+
+export default UserProfile;
